1afc103f61a5e2fe139a41b0dfd98e773bef6693
Simon <grytysek@gmail.com>
2014-07-07 23:52:36 +0200

    upgrades

--- jquery.line.js
+++ jquery.line.js
@@ -1,37 +1,138 @@
 /**
  * @author Szymon Działowski
  * @homepage https://bitbucket.org/stopsopa/jquery.line
- * @ver 1.0
+ * @ver 1.0 2014-07-06
+ * @ver 1.1 2014-07-07 upgrades, corrections
  */
 ;(function ($) {
+    // calc distance between two points
     function calcDistance(x1, y1, x2, y2) { // http://www.gwycon.com/calculating-the-distance-between-two-pixels/
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
-    function calcAngle(x1, y1, x2, y2) { // http://stackoverflow.com/a/7586218
-        return Math.atan( (y2 - y1) / (x2 - x1) ) * 180 / Math.PI;
+    // count angle in radians
+    function calcAngleRad(x1, y1, x2, y2) {
+        return Math.atan2( (y2 - y1) , (x2 - x1) );
+    }
+    // count angle in degrees
+    function calcAngle(x1, y1, x2, y2) {
+        var a = calcAngleRad(x1, y1, x2, y2) * (180 / Math.PI); 
+        return (a < 0) ? (a += 360) : a;
+    }
+    // count correction
+    function correct(x1, y1, x2, y2, o, ang) {
+        ang || (ang = calcAngle(x1, y1, x2, y2));
+        
+        var hw  = o.width/2;
+        var hwo = hw// originial
+        
+        var sw = false;
+        
+        if ($.isNumeric(o.correct)) {
+            var c = o.correct;
+            switch(true) {
+                case o.correctpos == 'normal':
+                    hw += o.correct;
+                    break;
+                case o.correctpos == 'top':                    
+                    if (ang > 90 && ang < 270) {
+                        sw = true;
+                        c = 0 - Math.abs(c);   
+                    }                   
+                    break;
+                case o.correctpos == 'bottom':                   
+                    if (ang < 90 || ang > 270) {
+                        sw = true;
+                        c = 0 - Math.abs(c);   
+                    }
+                    break;
+                case o.correctpos == 'left':                   
+                    if (ang > 0 && ang < 180) {
+                        sw = true;
+                        c = 0 - Math.abs(c);  
+                    }
+                    break;
+                case o.correctpos == 'right':                    
+                    if (ang < 0 || ang > 180) {
+                        sw = true;
+                        c = 0 - Math.abs(c);  
+                    }
+                    break;
+            }
+            hw += c;
+        }
+        
+        var rad = calcAngleRad(x1, y1, x2, y2); // radians
+        var radminhalf = (rad - (Math.PI / 2));      // radians minus half radian
+        
+        var c = {};
+        if (o.correct === false) {
+            c.x = 0;
+            c.y = 0;
+        }
+        else {
+            c.y = Math.sin(radminhalf)  * hw;
+            c.x = Math.cos(radminhalf)  * hw;
+            c.oy = Math.sin(radminhalf) * hwo; // without correct
+            c.ox = Math.cos(radminhalf) * hwo; // without correct
+        }
+        c.ang = ang;
+        c.rad = rad
+        c.ox2 = c.ox * 2;
+        c.oy2 = c.oy * 2;
+        c.x1 = x1;
+        c.y1 = y1;
+        c.x2 = x2;
+        c.y2 = y2;
+        
+        if (sw) {  
+            c.a1 = c.x1 - c.ox2;
+            c.a2 = c.y1 - c.oy2;
+            
+            c.b1 = c.a1 + c.x;
+            c.b2 = c.a2 + c.y;
+
+            c.c1 = c.x2 + c.x
+            c.c2 = c.y2 + c.y;
+
+            c.d1 = c.c1 - c.ox2;
+            c.d2 = c.c2 - c.oy2;
+        }
+        else {            
+            c.b1 = c.x1 + c.x;
+            c.b2 = c.y1 + c.y;
+
+            c.a1 = c.b1 - c.ox2;
+            c.a2 = c.b2 - c.oy2;
+
+            c.d1 = c.x2 + c.x;
+            c.d2 = c.y2 + c.y;
+
+            c.c1 = c.d1 - c.ox2
+            c.c2 = c.d2 - c.oy2;
+        }
+        
+        return c;
     }
     /**
      * @returns jQuery object representing line
      */
     $.fn.line = function (x1, y1, x2, y2, opt, callback) {
         
-        if (x2 < x1) {
-            var e = x2;
-            x2 = x1;
-            x1 = e;
-            e = y1;
-            y1 = y2;
-            y2 = e;
-        }
+        opt || (opt = {});
 
         if ($(this).length > 1) 
             throw "$(this) is more then one element";            
             
         var o = $.extend(true, {
+            style: 'solid',
+            width: 1,
+            color: 'black',
+            
             cls: '_line',
             id: false,
+            correct: true, // bool|int - corection of position, give integer to move 
+            correctpos: 'normal', // normal, top, bottom, left, right
             css: {
-                borderTop: '1px solid black',
                 height: '0',
                 zIndex: '999',
                 zoom: 1
@@ -41,33 +142,36 @@
         o.create = opt.create ? $(opt.create) : $('<div></div>');
         opt.css && (o.css = opt.css);
 
-        var angle = calcAngle(x1, y1, x2, y2);
-
+        var ang  = calcAngle(x1, y1, x2, y2);    // degrees
+        
+        var c = correct(x1, y1, x2, y2, o/* half of line width */, ang)     
+        
         o.create
-            .css(o.css)
             .css({
+                borderTop: o.width+'px '+o.style+' '+o.color,
                 position: 'absolute',
                 width: calcDistance(x1, y1, x2, y2) + 'px',
-                '-webkit-transform': 'rotate(' + angle + 'deg)',
-                '-moz-transform': 'rotate(' + angle + 'deg)',
-                '-ms-transform': 'rotate(' + angle + 'deg)',
-                '-o-transform': 'rotate(' + angle + 'deg)',
-                transform: 'rotate(' + angle + 'deg)',
+                '-webkit-transform': 'rotate(' + ang + 'deg)',
+                '-moz-transform': 'rotate(' + ang + 'deg)',
+                '-ms-transform': 'rotate(' + ang + 'deg)',
+                '-o-transform': 'rotate(' + ang + 'deg)',
+                transform: 'rotate(' + ang + 'deg)',
 
                 'transform-origin' :  '0 0',
                 '-ms-transform-origin' : '0 0', /* IE 9 */
                 '-webkit-transform-origin' : '0 0', /* Chrome, Safari, Opera */                
 
-                top : y1+'px',
-                left : x1+'px'
-            });
+                top : (y1 + c.y)+'px',
+                left : (x1 + c.x)+'px'
+            })
+            .css(o.css);
 
         o.cls && o.create.addClass(o.cls);
         o.id && o.create.attr('id', o.id);
 
         o.create.appendTo(this);
         
-        (typeof callback == 'function') && callback(o.create, o);
+        (typeof callback == 'function') && callback(o.create, o, c);
         
         return o.create;
     };
